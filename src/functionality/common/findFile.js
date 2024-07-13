8205139126bdfaf0ddc0c4dc5b7e7025681c3aee
christopher-k-c <chriscareyk@gmail.com>
2024-07-13 18:42:10 +0100

    findFile func re-write/package-lock replace

--- src/functionality/common/findFile.js
+++ src/functionality/common/findFile.js
@@ -4,67 +4,33 @@ const fs = require('uxp').storage.localFileSystem;
 async function findFile(arr, openFile){
 
     try {
-
         const match = arr.find((element) => {
-            // Part of file name variable 
-            // str.slice(0, str.lastIndexOf('_'));
 
-            console.log(element, "tetstststs")
-            let str = element.name
-            console.log(str, "tetstststs")
-            let fromUnderScore = str.slice(0, str.lastIndexOf('_'));
-            let fromDot = str.slice(0, str.lastIndexOf('.'))
-            let fromM0 = str.slice(0, str.lastIndexOf('M0'))
+            let noteName = element.name
+            let openName = openFile.name
 
-            console.log(fromUnderScore, "fromUnderScore")
-            console.log(fromDot, "fromDot")
-            console.log(fromM0, "fromM0")
-            // select from the _ up to but not including the dot
+            // Remove all from underscore 
+            let slicedNoteName = noteName.slice(0, noteName.lastIndexOf('_'));
+            let slicedOpenName = openName.slice(0, openName.lastIndexOf('_'));
+            // Remove from .
+            let noteNameSlicedFromDot = openName.slice(0, openName.lastIndexOf('.'))
+            // Remove from M0
+            let openNameSlicedFromM0 = noteName.slice(0, noteName.lastIndexOf('M0'))
 
-            // file extension variable 
-            // select from the . onwards 
-
-            // In first replace call append both var1 + var2 
-            // In second replace call, var2 (the extension) to be replaced by nothing
-            // Same for the other one or a conditional for the swatches and _r but currently very short of time 
-        
-
-
-            if(element.name.replace(fromUnderScore, "") === openFile.name.replace(fromDot, "") 
-                || element.name.replace(fromM0, "") === openFile.name.replace(fromDot, ""))
-            {
-                console.log(element, "tettststst")
-                return element.nativePath; // Return the path
-
-                // console.log(element.nativePath)
+            if(noteName.includes("M0") && openNameSlicedFromM0 === noteNameSlicedFromDot){
+                return element.nativePath;
+            } else if (noteName.includes("_") && slicedNoteName === slicedOpenName){
+                return element.nativePath;
             }
-
-            // This needs to be cleaned up, needs to handle multiple file types etc
-
-
-            if(element.name.replace("_swatches.tif", "") === openFile.name.replace(".tif", "") 
-                || element.name.replace("M0_swatches.tif", "") === openFile.name.replace(".tif", ""))
-            {
-                return element.nativePath; // Return the path
-
-                // console.log(element.nativePath)
-            }
-
-
-
         })
 
         return match // Return an object if theres a match 
 
-
     }
     catch (error) {
         console.log(error)
+        return false
     }
-
-
-
-
 }
 
 export default findFile;
