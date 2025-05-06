2ba301f58652f6209a23d954f22a2819dc844a6d
Chris Carey <chriscarey@dt014709.newlook.co.uk>
2025-05-06 17:40:37 +0100

    supabase-auth-integration update

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import UserMetrics from "./UserMetrics";
+// import UserMetrics from "./UserMetrics";
 import { entrypoints } from "uxp";
 import appPanel from './css/App.modules.css'
 import metricsPanel from './css/UserMetrics.modules.css'
+import { WorkSubmissions } from "./functionality/collector";
 
 
 // Multi-Panel Set-up
@@ -23,19 +24,19 @@ entrypoints.setup({
         ReactDOM.render(<App />, root);
       },
     },
-    // secondPanel: {
-    //   show(body) {
-    //     while (body.firstChild) {
-    //       body.removeChild(body.firstChild);
-    //     }
+    secondPanel: {
+      show(body) {
+        while (body.firstChild) {
+          body.removeChild(body.firstChild);
+        }
         
-    //     const root = document.createElement("div");
-    //     root.id = "root-metrics"; 
-    //     root.className = metricsPanel.rootContainer; 
-    //     body.appendChild(root);
-    //     ReactDOM.render(<UserMetrics />, root);
-    //   },
-    // },
+        const root = document.createElement("div");
+        root.id = "root-metrics"; 
+        root.className = metricsPanel.rootContainer; 
+        body.appendChild(root);
+        ReactDOM.render(<WorkSubmissions />, root);
+      },
+    },
 
     // this section is the reason i have two manifest.json files, dev being the one which runs with this code
   },
